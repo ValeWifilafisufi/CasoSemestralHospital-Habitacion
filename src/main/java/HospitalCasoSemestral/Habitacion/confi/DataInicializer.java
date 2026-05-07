e29fd4dec08a22309c9ccb1eb5cb302a781e9c64
ValeWifilafisufi <val.castroo@duocuc.cl>
2026-05-07 00:09:34 -0400

    se termina el data

--- src/main/java/HospitalCasoSemestral/Habitacion/confi/DataInicializer.java
+++ src/main/java/HospitalCasoSemestral/Habitacion/confi/DataInicializer.java
@@ -1,14 +1,21 @@
 package HospitalCasoSemestral.Habitacion.confi;
 
+import HospitalCasoSemestral.Habitacion.model.Habitacion;
 import HospitalCasoSemestral.Habitacion.repository.HabitacionRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class DataInicializer {
+public class DataInicializer implements CommandLineRunner {
 
     private final HabitacionRepository habitacionRepository;
 
@@ -19,8 +26,10 @@ public class DataInicializer {
             return;
         }
         log.info(">>>DataInitializer: BD vacia detextada, insertando datos de prueba...");
-
-        log.info(">>>DataInitializer: {} productos insertados correctamente.",
-                productoRepository.count());
+        habitacionRepository.save(new Habitacion(123L, 2L, 4L, "UCI", "Desocupadas", new BigDecimal("3993.3")));
+        log.info(">>>DataInitializer: {} habitaciones insertadas correctamente.",
+                habitacionRepository.count());
     }
+    }
+
 }
