852a4c7e66bfc1b660c4fd34543400855ce0c8f7
ValeWifilafisufi <val.castroo@duocuc.cl>
2026-06-08 18:09:03 -0400

    se trata de agregar eureka y se ordenan las dependencias

--- src/main/java/HospitalCasoSemestral/Habitacion/HabitacionApplication.java
+++ src/main/java/HospitalCasoSemestral/Habitacion/HabitacionApplication.java
@@ -2,7 +2,9 @@ package HospitalCasoSemestral.Habitacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class HabitacionApplication {
 
